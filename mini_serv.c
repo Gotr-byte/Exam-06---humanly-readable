de6016fdb0e34b39d17f1446d0036571c4bd7999
Piotr Biedermann <piotr.biedermann84@gmail.com>
2024-02-26 14:42:19 +0100

    Naming is more readable, added commentaries

--- mini_serv.c
+++ mini_serv.c
@@ -13,7 +13,7 @@ The struct contains a client id and a char array of the message.
 typedef struct s_client
 {
     int id;
-    char msg[696969];
+    char msg[999999];
 }   t_client;
 
 /*
@@ -23,7 +23,7 @@ t_client clients[128];
 /*
 Integer values of file descriptors and possibly the id-s of next client.
 */
-int socket_fd = 0, connection_fd = 0, maximum_fd = 0, idNext = 0;
+int socket_fd = 0, connection_fd = 0, maximum_fd = 0, id_next = 0;
 /*
 The fd_set variable type used in select.
 */
@@ -35,7 +35,7 @@ struct sockaddr_in server_address, client_address;
 /*
 Arrays to store the bytes received and the bytes to be sent.
 */
-char read_buffer[696969], write_buffer[696969];
+char read_buffer[999999], write_buffer[999999];
 
 /*
 Helper function to display error messages.
@@ -76,9 +76,9 @@ int main(int argc, char **argv)
     if (argc != 2)
         error("Wrong number of arguments\n");
 	/*
-	socket create and verification
+	Socket create and verification.  int socket(int domain, int type, int protocol);
 	*/
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0); 
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd == -1)
 		error(NULL);
     maximum_fd = socket_fd;
@@ -90,7 +90,7 @@ int main(int argc, char **argv)
 	assign IP, PORT
 	*/
     int port = atoi(argv[1]);
-	server_address.sin_family = AF_INET; 
+	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	server_address.sin_port = htons(port); 
 	/*
@@ -100,26 +100,39 @@ int main(int argc, char **argv)
 		error(NULL);
 	if (listen(socket_fd, 128) != 0)
 		error(NULL);
+	/*
+	Clear all entries from the set.
+	*/
     FD_ZERO(&active_fds);
+	/*
+	Add fd to the set.
+	*/
     FD_SET(socket_fd, &active_fds);
-	
-	while (6969)
+	/*
+	The server loop.
+	*/
+	while (1)
 	{
 		read_fds = write_fds = active_fds;
 		if (select(maximum_fd + 1, &read_fds, &write_fds, NULL, NULL) < 0)
 			continue;
 		for (int fd_id = 0; fd_id <= maximum_fd; fd_id++)
 		{
+			/*
+			FD_ISSET - return true if fd is in the set. If there is activity on the listening_socket,
+			create a socket to listen. The server listening socket receives bytes and adds the file 
+			descriptor to the listened fd list.
+			*/
 			if (FD_ISSET(fd_id, &read_fds) && fd_id == socket_fd)
 			{
 				connection_fd = accept(socket_fd, (struct sockaddr *)&client_address, &len);
 				if (connection_fd < 0)
 					continue;
 				maximum_fd = connection_fd > maximum_fd ? connection_fd : maximum_fd;
-				sprintf(write_buffer, "server: client %d just arrived\n", idNext);
+				sprintf(write_buffer, "server: client %d just arrived\n", id_next);
 				send_all(connection_fd);
 				bzero(&write_buffer, strlen(write_buffer));
-				clients[connection_fd].id = idNext++;
+				clients[connection_fd].id = id_next++;
 				FD_SET(connection_fd, &active_fds);
 			}
 			else if (FD_ISSET(fd_id, &read_fds))
